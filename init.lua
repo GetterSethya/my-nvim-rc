f9ad8a968d607fdbfcdf9dc057868ae53e022a0c
Getter Sethya <gettersethya@gmail.com>
2024-04-13 12:17:49 +0700

    fix keymap

--- init.lua
+++ init.lua
@@ -47,7 +47,7 @@ vim.keymap.set("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<cr>", { no
 
 vim.keymap.set("n", "gl", "<cmd>lua vim.diagnostic.open_float()<cr>", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-p>", "<cmd>lua vim.diagnostic.goto_prev()<cr>", { noremap = true, silent = true })
-vim.keymap.set("n", "<C-n>", "<cmd>lua vim.diagnostic.goto_next()<cr>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-[>", "<cmd>lua vim.diagnostic.goto_next()<cr>", { noremap = true, silent = true })
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -35,5 +35,54 @@ lspconfig.astro.setup {
         client.server_capabilities.documentFormattingProvider = true
     end,
 }
+lspconfig.rust_analyzer.setup {
+    on_attach = function(client)
+        client.server_capabilities.documentFormattingProvider = true
+    end,
+    settings = {
+        ["rust-analyzer"] = {
+            imports = {
+                granularity = {
+                    group = "module",
+                },
+                prefix = "self",
+            },
+            cargo = {
+                buildScripts = {
+                    enable = true,
+                },
+            },
+            procMacro = {
+                enable = true,
+            },
+        },
+    },
+}
+
+-- lspconfig.emmet_language_server.setup {
+--     filetypes = { "css", "html", "javascript", "javascriptreact", "typescriptreact" },
+--     -- Read more about this options in the [vscode docs](https://code.visualstudio.com/docs/editor/emmet#_emmet-configuration).
+--     -- **Note:** only the options listed in the table are supported.
+--     init_options = {
+--         ---@type table<string, string>
+--         includeLanguages = {},
+--         --- @type string[]
+--         excludeLanguages = {},
+--         --- @type string[]
+--         extensionsPath = {},
+--         --- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/preferences/)
+--         preferences = {},
+--         --- @type boolean Defaults to `true`
+--         showAbbreviationSuggestions = true,
+--         --- @type "always" | "never" Defaults to `"always"`
+--         showExpandedAbbreviation = "always",
+--         --- @type boolean Defaults to `false`
+--         showSuggestionsAsSnippets = false,
+--         --- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/syntax-profiles/)
+--         syntaxProfiles = {},
+--         --- @type table<string, string> [Emmet Docs](https://docs.emmet.io/customization/snippets/#variables)
+--         variables = {},
+--     },
+-- }
 
 -- lspconfig.pyright.setup { blabla}
